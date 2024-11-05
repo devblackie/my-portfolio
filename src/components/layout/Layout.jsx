7dc42ced28eb676099b9828989857a9158dfabcc
Newton Mboi <newtieblackie@gmail.com>
2024-11-06 00:52:55 +0300

    Linked MobileFooter to Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,17 +1,17 @@
 import { Routers } from "../../routers/Routers";
+import MobileFooter from "../../shared/components/MobileFooter";
 import Header from "../header/Header";
 import Sidebar from "../sidebar/Sidebar";
 
 const Layout = () => {
   return (
     <>
-      <div>
         <Header />
         <div className="flex flex-1">
           <Sidebar />
           <Routers />
         </div>
-      </div>
+        <MobileFooter />
     </>
   );
 };
