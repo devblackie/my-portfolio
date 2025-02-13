38765ea3584ab0409f5e0fb35babb7c8d16e3a54
Newton Mboi <newtieblackie@gmail.com>
2025-02-13 18:12:31 +0300

    Typewritter effect removed

--- src/data/navigation/SidebarData.jsx
+++ src/data/navigation/SidebarData.jsx
@@ -1,14 +1,24 @@
-import { FcBriefcase, FcContacts,  FcHome,  FcManager} from "react-icons/fc";
+import { FcBriefcase, FcContacts,  FcHome} from "react-icons/fc";
 
 const SidebarData = [
   { path: "/", display: "Home", icon: <FcHome /> },
   { path: "/portfolio", display: "Portfolio", icon: <FcBriefcase /> },
-  { path: "/core", display: "Services", icon: <FcManager /> },
+  { path: "/core", display: "Services", icon: (  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24" width="24">
+    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1.5" stroke="currentColor" d="M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21M23 21V19C22.9993 18.1137 22.7044 17.2528 22.1614 16.5523C21.6184 15.8519 20.8581 15.3516 20 15.13M16 3.13C16.8604 3.3503 17.623 3.8507 18.1676 4.55231C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89317 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88M13 7C13 9.20914 11.2091 11 9 11C6.79086 11 5 9.20914 5 7C5 4.79086 6.79086 3 9 3C11.2091 3 13 4.79086 13 7Z"></path>
+  </svg>) },
   {
     path: "/contact",
     display: "Contact",
     icon: <FcContacts />,
   },
 ];
-
+{/* <button
+onClick={onClick}
+className="cursor-pointer group/download relative flex gap-1 px-8 py-2 bg-[#5c5fe9] text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 font-semibold shadow-xl active:shadow-inner transition-all duration-300"
+>
+{name}
+<div className="absolute text-xs uppercase scale-0 rounded-md py-2 px-2 bg-[#5c5fe9] left-2/4 mb-3 bottom-full group-hover/download:scale-100 origin-bottom transition-all duration-300 shadow-lg before:content-[''] before:absolute before:top-full before:left-2/4 before:w-3 before:h-3 before:border-solid before:bg-[#5c5fe9] before:rotate-45 before:-translate-y-2/4 before:-translate-x-2/4">
+  ksh {price}
+</div>
+</button> */}
 export default SidebarData;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,5 @@
 import {
-  FaCode,
+  // FaCode,
   FaDatabase,
   FaLayerGroup,
   FaObjectUngroup,
@@ -76,7 +76,13 @@ export const About = () => {
                 </div>
                 <div className="p-4 rounded-lg shadow-lg bg-[#b8e0fd] text-[#0000ff] flex flex-1">
                   <div className=" text-3xl">
-                    <FaCode />
+                    {/* <FaCode /> */}
+                    <svg viewBox="0 0 640 512" height="5em" xmlns="http://www.w3.org/2000/svg" 
+                    fill="currentColor" className="h-7">
+      <path
+        d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"
+      ></path>
+    </svg>
                   </div>
                   <div className="ml-4 text-masterBlack tracking-wider">
                     <div className="text-sm font-extrabold capitalize">
